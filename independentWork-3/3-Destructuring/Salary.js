eedbdae7650730548d61f6c18609a0b1ddebab20
Igor Shleht <imacelebrity1@gmail.com>
2021-11-15 16:36:32 +0200

    Create Salary.js

---
+++ independentWork-3/3-Destructuring/Salary.js
@@ -0,0 +1,29 @@
+/*
+У нас есть объект salaries с зарплатами:
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+Создайте функцию topSalary(salaries), которая возвращает имя самого высокооплачиваемого сотрудника.
+
+Если объект salaries пустой, то нужно вернуть null.
+Если несколько высокооплачиваемых сотрудников, можно вернуть любого из них.
+P.S. Используйте Object.entries и деструктурирование, чтобы перебрать пары ключ/значение.
+*/
+
+function topSalary(salaries) {
+
+  const max = 0;
+  const maxName = null;
+
+  for(const [name, salary] of Object.entries(salaries)) {
+    if (max < salary) {
+      max = salary;
+      maxName = name;
+    }
+  }
+
+  return `Сотрудник ${maxName} имеет самую высокую зарплату ${max} среди остальных`;
+}

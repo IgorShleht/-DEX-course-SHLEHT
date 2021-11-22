1450815b0e3e636a9052b025b1ee29bcbd3cda05
Igor Shleht <imacelebrity1@gmail.com>
2021-11-22 16:29:26 +0200

    Create instanceof.js

---
+++ independentWork-5/instanceof.js
@@ -0,0 +1,14 @@
+/*
+Почему instanceof в примере ниже возвращает true? Мы же видим, что a не создан с помощью B().
+
+function A() {}
+function B() {}
+
+A.prototype = B.prototype = {};
+
+let a = new A();
+
+alert( a instanceof B ); // true
+*/
+
+// instanceof учитывает только prototype

3b92ade552689e0c7932bdb31a98da505d133559
Igor Shleht <imacelebrity1@gmail.com>
2021-12-06 13:09:11 +0200

    Create TodoCard.tsx

---
+++ homeWork-9/components/TodoCard.tsx
@@ -0,0 +1,26 @@
+import { ITodo } from "../types";
+
+interface TodoCardProps {
+  todo: ITodo;
+  deleteCallback: (id: number) => void;
+  id: number;
+}
+
+//TODO: сделать компонент TodoCard
+export const TodoCard: React.FC<TodoCardProps> = ({
+  todo,
+  deleteCallback,
+  id
+}) => {
+  const handleDelete = () => {
+    deleteCallback(id);
+  };
+  return (
+    <li className="todoCard">
+      <p>{todo.text}</p>
+      <button className="delete_todo" onClick={handleDelete}>
+        X
+      </button>
+    </li>
+  );
+};

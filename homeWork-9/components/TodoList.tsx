8546902b1a7fa82d99c05fa7d030a2c5f38e8de7
Igor Shleht <imacelebrity1@gmail.com>
2021-12-06 13:09:26 +0200

    Create TodoList.tsx

---
+++ homeWork-9/components/TodoList.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { ITodo } from "../types";
+import { TodoCard } from "./TodoCard";
+
+interface ITodoListProps {
+  todos: ITodo[];
+  deleteCallback: (id: number) => void;
+}
+
+//TODO: сделать TodoList компонент
+export const TodoList: React.FC<ITodoListProps> = ({
+  todos,
+  deleteCallback
+}) => {
+  return (
+    <ul>
+      <p>{todos.length ? "Список" : "Пусто"}</p>
+      {todos.map((todo) => (
+        <TodoCard
+          id={todo.id}
+          key={todo.id}
+          todo={todo}
+          deleteCallback={deleteCallback}
+        />
+      ))}
+    </ul>
+  );
+};
